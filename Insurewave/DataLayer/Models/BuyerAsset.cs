05021aaf2abc16710d2877cde65950926ba345ca
Jisu <jisumaji@gmail.com>
2022-04-18 10:51:45 +0530

    DataAnnotations

--- Insurewave/DataLayer/Models/BuyerAsset.cs
+++ Insurewave/DataLayer/Models/BuyerAsset.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 #nullable disable
 
 namespace DataLayer.Models
@@ -15,9 +15,13 @@ namespace DataLayer.Models
 
         public int AssetId { get; set; }
         public string UserId { get; set; }
+        [Required(ErrorMessage ="Country cannot be null")]
         public int? CountryId { get; set; }
+        [Required(ErrorMessage ="Please enter an asset name")]
         public string AssetName { get; set; }
+        [Required(ErrorMessage ="Please enter a price for your asset")]
         public decimal PriceUsd { get; set; }
+        [Required(ErrorMessage ="Please enter a type")]
         public string Type { get; set; }
         public string Request { get; set; }
 
